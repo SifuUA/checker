95be59d5f2bb821fae26ee38d90d39121e2bffa5
okres <alekseykres@gmail.com>
2017-02-26 16:57:25 +0200

    checker

---
+++ checker/sa_sb_ss.c
@@ -0,0 +1,34 @@
+#include "checker.h"
+
+void    sa(t_d_linklst *list_a)
+{
+	T tmp;
+
+	if (list_a->size > 1)
+	{
+		tmp = list_a->head->value;
+		list_a->head->value = list_a->head->next->value;
+		list_a->head->next->value = tmp;
+		put("sa");
+	}
+}
+
+void    sb(t_d_linklst *list_b)
+{
+	T tmp;
+
+	if (list_b->size > 1)
+	{
+		tmp = list_b->head->value;
+		list_b->head->value = list_b->head->next->value;
+		list_b->head->next->value = tmp;
+		put("sb");
+	}
+}
+
+void    ss(t_d_linklst *list_a, t_d_linklst *list_b)
+{
+	sa(list_a);
+	sb(list_b);
+	put("ss");
+}
